b21119fcb18a78cc8f4d252b4f69c899e32c38f9
VladTrygub <vladtrygub83@gmail.com>
2016-04-02 15:36:19 +0300

    create TestCategoryService.java for test category select query from db

---
+++ src/test/java/com/my/shop/dao/TestCategoryService.java
@@ -0,0 +1,29 @@
+package com.my.shop.dao;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.my.shop.model.Category;
+import com.my.shop.service.CategoryService;
+import com.my.shop.service.CategoryServiceImpl;
+
+public class TestCategoryService {
+
+	@Test
+	public void test() {
+		ApplicationContext context = new ClassPathXmlApplicationContext("rootContext.xml");
+		CategoryService categoryService = (CategoryService) context.getBean("categoryService");
+		List<Category> categories = categoryService.getAllCategories();
+		for (Category category : categories) {
+			System.out.println(category.getName());
+		}
+		assertEquals(3, categories.size());
+	}
+
+}
