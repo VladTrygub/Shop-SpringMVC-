81e5cb2e75662f7b89f6731813f3a864e8ff96fe
VladTrygub <vladtrygub83@gmail.com>
2016-04-07 18:40:40 +0300

    implemented testCreateCustomer()
    added init() method

--- src/test/java/com/my/shop/dao/TestCustomerService.java
+++ src/test/java/com/my/shop/dao/TestCustomerService.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import java.util.List;
 
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -13,21 +15,42 @@ import com.my.shop.service.CustomerService;
 
 public class TestCustomerService {
 	
+	private ApplicationContext context;
+	private CustomerService customerService;
 	
+	@Before
+	public void init() {
+		ApplicationContext context = new ClassPathXmlApplicationContext("rootContext.xml");
+		customerService = (CustomerService) context.getBean("customerService");
+	}
 
 	@Test
+	@Ignore
 	public void testGetAllCustomers() {
-		ApplicationContext context = new ClassPathXmlApplicationContext("rootContext.xml");
-		CustomerService customerService = (CustomerService) context.getBean("customerService");
 		List<Customer> customers = customerService.getAllCustomers();
-		
+		for (Customer customer : customers) {
+			System.out.println(customer.getId());
+		}
 		assertFalse(customers.isEmpty());
 		assertNotNull(customers);
 	}
 	
 	@Test
+	@Ignore
 	public void testGetCustomerByID() {
-		
+		Customer customer = customerService.getCustomerByID(1);
+		assertEquals("log1", customer.getLogin());
+	}
+	
+	@Test
+	@Ignore
+	public void testCreateCustomer() {
+		Customer c = new Customer();
+		c.setLogin("testLog");
+		c.setPassword("testPass");
+		customerService.createCustomer(c);
+		System.out.println(c.getId());
+		assertNotSame(0, c.getId());
 	}
 
 }
